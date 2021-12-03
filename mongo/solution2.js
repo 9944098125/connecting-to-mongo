94467d2ad0c6a25dc3ac20e5688dcd6d1eefd194
Srinivas Akella <srinivas72075@gmail.com>
2021-12-03 12:53:24 +0530

    This is the code regarding connecting to mongo db

---
+++ mongo/solution2.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/mongo-exercises");
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: Date,
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function getCourses() {
+  return await Course.find({
+    isPublished: true,
+    tags: { $in: ["frontend", "backend"] },
+  })
+    .sort({ price: -1 }) // or we can pass a string here like (-price)
+    .select({ name: 1, author: 1, price: 1 }); // or ('name author price')
+}
+
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+
+run();
